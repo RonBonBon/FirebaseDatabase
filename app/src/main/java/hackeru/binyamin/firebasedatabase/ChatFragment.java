a9fc9e5b1af9cd70d617b5a5289f7bad037dbe58
hackerudroids <hackerudroids@gmail.com>
2017-06-19 18:25:24 +0300

    Ver 01.13

--- app/src/main/java/hackeru/binyamin/firebasedatabase/ChatFragment.java
+++ app/src/main/java/hackeru/binyamin/firebasedatabase/ChatFragment.java
@@ -3,9 +3,20 @@ package hackeru.binyamin.firebasedatabase;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 
 /**
@@ -13,6 +24,14 @@ import android.view.ViewGroup;
  */
 public class ChatFragment extends Fragment {
 
+
+    @BindView(R.id.etMessage)
+    EditText etMessage;
+    @BindView(R.id.btnSend)
+    Button btnSend;
+    @BindView(R.id.rvChat)
+    RecyclerView rvChat;
+    Unbinder unbinder;
 
     public ChatFragment() {
         // Required empty public constructor
@@ -23,7 +42,24 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.btnSend)
+    public void onViewClicked() {
+        //get reference to a table in the database
+        DatabaseReference chatTable = FirebaseDatabase.getInstance().getReference("Chat");
+        //add a new row to the table
+        DatabaseReference newRow = chatTable.push();
+        //setValue(et.getText().toString())
+        newRow.setValue(etMessage.getText().toString());
+    }
 }
